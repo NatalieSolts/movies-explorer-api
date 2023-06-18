f9ca5e9587d1bb104fbe2ce73a8701854923d61d
Natalie <mrs.solts@mail.ru>
2023-06-18 13:03:25 +0500

    обновление файлов

--- routes/signup.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { createUser } = require('../controllers/users');
-const { userSignUpValidate } = require('../middlewares/validators/userValidators');
-
-router.post('/', userSignUpValidate, createUser);
-
-module.exports = router;
